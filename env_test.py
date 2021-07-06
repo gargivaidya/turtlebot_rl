086dd6b3fc78dbe3e94dc660ce06ee31a7ddf436
Gargi Vaidya <gargivaidya@tamu.edu>
2021-07-06 08:37:28 -0500

    Final script for testing python environments

--- env_test.py
+++ env_test.py
@@ -1,43 +1,39 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import math
-import gym
-from gym import spaces
-import time
+
 import itertools
 import argparse
-import datetime
-import random
+from agent import ContinuousDubinGym, DiscreteDubinGym
 from stable_baselines.common.env_checker import check_env
 
 parser = argparse.ArgumentParser(description='PyTorch Soft Actor-Critic Args')
 parser.add_argument('--env-name', default="ContinuousDubinGym",
-                    help='Dubin Gym environment (default: ContinuousDubinGym)')
+					help='Dubin Gym environment (default: ContinuousDubinGym)')
 args = parser.parse_args()
 
 def main():
 	
-	if args.env-name == ContinuousDubinGym:
-		env =  ContinuousDubinGymDubinGym()
+	if args.env_name == "ContinuousDubinGym":
+		env =  ContinuousDubinGym()
 	else:
 		env = DiscreteDubinGym()
 
-	print(check_env(env))
+	print("Issues with Custom Environment : ", check_env(env))
+
+	print("Testing sample action...")
 
 	max_steps = int(1e6)
 	state = env.reset()
-	env.render()
 	for ep in range(3):
 		state = env.reset()
-		env.render()
 		for i in range(max_steps):
-			action = [1.0, 0.]
-	 		n_state,reward,done,info = env.step(action)
-	 		env.render()
-	 		if done:
-	 			state = env.reset()
-	 			done = False                   
-	 			break
+			if args.env_name == "ContinuousDubinGym":
+				action = [1., 0.]
+			else:
+				action = 7			
+			n_state,reward,done,info = env.step(action)
+			env.render()
+			if done:
+				done = False                   
+				break
 
 if __name__ == '__main__':
 	main()
